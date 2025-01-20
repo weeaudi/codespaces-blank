78141a0fcd2bd5ada03fb4a980aaef9bcfa3aed9
weeaudi <gurskaideng00@isd728.org>
2025-01-19 19:37:52 -0600

    add doxygen support (will add cmake compatability latter)

--- src/bootloader/stage1/boot.asm
+++ src/bootloader/stage1/boot.asm
@@ -1,4 +1,14 @@
-; boot.asm - A simple bootloader that prints "Hello, World!"
+;;
+; This file is the entry point to the bootloader. It will load the second stage and jump to it.
+; @include{lineno} boot.asm
+; @author Aidcraft
+; @version 0.0.2
+; @date 2025-01-19
+; 
+; @copyright Copyright (c) 2025 Aiden Gursky. All Rights Reserved
+; @par License:
+; This project is released under the Artistic License 2.0
+;;
 
 bits 16
 
@@ -11,48 +21,47 @@ section .fsjump
 
 section .text
 
+;;
+; @brief Entry point of the bootloader
+; @details Initializes data segments, sets up the stack, prints a message, and enters an infinite loop to hang the system.
+;;
 start:
-
-    ; setup data segments
-    mov ax, 0                   ; can't set ds/es directly
+    mov ax, 0
     mov ds, ax
     mov es, ax
-    
-    ; setup stack
     mov ss, ax
-    mov sp, 0x7C00              ; stack grows downwards from where we are loaded in memory
+    mov sp, 0x7C00
+    mov si, message
+    call print_string
 
-    ; Print "Hello, World!"
-    mov si, message  ; Load the address of the message
-    call print_string ; Call the function to print the string
-
-    ; Hang the system (infinite loop)
 hang:
     jmp hang
 
-; Function to print a string
+;;
+; @brief Function to print a string
+; @details Prints a null-terminated string using BIOS interrupt 0x10.
+; @param[in] si Address of the null-terminated string to print
+;;
 print_string:
-
-    push si         ; save registers we modify
+    push si
     push ax
     push bx
 
 .next_char:
-    lodsb            ; Load byte at DS:SI into AL and increment SI
-    or al, al           ; verify if next character is null?
+    lodsb
+    or al, al
     jz .done
-    mov ah, 0x0E    ; BIOS teletype function (write character)
+    mov ah, 0x0E
     mov bh, 0
-    int 0x10        ; Call BIOS interrupt to print character
-    jmp .next_char  ; Repeat for the next character
-.done:
+    int 0x10
+    jmp .next_char
 
-    pop bx          ; set the registers back
+.done:
+    pop bx
     pop ax
     pop si
+    ret
 
-    ret              ; Return from the function
-
-message db 'Hello, World!', 0  ; Null-terminated string
+message db 'Hello, World!', 0
 
 section bios_footer
