6676df8e5cb46f56d6cc57225a9279c596c945c2
weeaudi <48772079+weeaudi@users.noreply.github.com>
2025-03-04 09:52:51 -0600

    Fix dependency issues

--- apt_installer.sh
+++ apt_installer.sh
@@ -1,4 +1,4 @@
 apt update
-apt install libmpc-dev libgmp-dev cmake texinfo yasm qemu-system-x86-64 python3-parted libguestfs-tools doxygen python3-parted libparted-dev -y
-pip3 install pyparted sh pyelftools
+apt install libmpc-dev libgmp-dev cmake texinfo nasm qemu-system-x86-64 python3-parted libguestfs-tools doxygen python3-parted libparted-dev -y
+pip3 install pyparted sh pyelftools PyFatFS
 chmod +r /boot/vmlinuz-*
